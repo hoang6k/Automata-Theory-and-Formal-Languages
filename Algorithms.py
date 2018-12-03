d0f4ba3d281b6017d9d5f960428269fffc365186
Hoàng Sáu Ka <daohoang.hust@gmail.com>
2018-12-03 21:08:17 +0700

    add Gt_to_NFA

--- Algorithms.py
+++ Algorithms.py
@@ -1,9 +1,10 @@
-import NFA_to_DFA, Minimize_DFA, Gp_to_NFA, NFA_to_Gp, G_to_Gc, Derivation_Tree
+import NFA_to_DFA, Minimize_DFA, Gt_to_NFA, Gp_to_NFA, NFA_to_Gp, G_to_Gc, Derivation_Tree
 
 """
 NFA_to_DFA.main()
 Minimize_DFA.main()
 
+Gt_to_NFA.main()
 Gp_to_NFA.main()
 NFA_to_Gp.main()
 G_to_Gc.main()
